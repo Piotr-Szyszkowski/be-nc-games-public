3f4402075f13ae56f031f6bfa3af3dea7fb15573
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2021-04-30 12:59:11 +0100

    attempt at heroku config

--- db/connection.js
+++ db/connection.js
@@ -5,9 +5,17 @@ const ENV = process.env.NODE_ENV || `development`;
 
 require(`dotenv`).config({ path: path.join(__dirname, `..`, `.env.${ENV}`) });
 
-if (!process.env.PGDATABASE) {
-  throw new Error(`PGDATABASE database not set`);
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error(`PGDATABASE or DATABASE_URL not set !!`);
 }
+
+const config =
+  ENV === `production`
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {};
 
 const db = new Pool();
 
