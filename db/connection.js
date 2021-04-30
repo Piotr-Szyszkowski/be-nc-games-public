923cb8a576a30bef3b701a51eff43ba1c0be2e80
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2021-04-30 15:31:29 +0100

    attempt at heroku config again

--- db/connection.js
+++ db/connection.js
@@ -10,10 +10,12 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 }
 
 const config =
-  ENV === `production`
+  ENV === "production"
     ? {
         connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
+        ssl: {
+          rejectUnauthorized: false,
+        },
       }
     : {};
 
